c685ca8dd0bef2111032e25f1bcbc297b0de4120
Victor-Borges-Silva <victorborges975@gmail.com>
2024-08-04 16:08:52 -0300

    ajuste na função python

--- lambda_function.py
+++ lambda_function.py
@@ -2,15 +2,10 @@ import boto3
 import os
 import json
 
-region = 'sa-east-1'
+region = 'us-west-1'
 instances = json.loads(os.environ['instancia_id'])
 ec2 = boto3.client('ec2', region_name=region)
 
 def lambda_handler(event, context): 
     ec2.stop_instances(InstanceIds=instances)
     print('stopped your instances: ' + str(instances))
-
-
-
-
-
